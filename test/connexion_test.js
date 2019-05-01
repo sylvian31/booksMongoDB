adaa342b022df72c843e63d1fefb54aa1864593b
sylvian <sylvian31@gmail.com>
2019-04-30 21:05:41 -0400

    add assert

--- test/connexion_test.js
+++ test/connexion_test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 
 before( (done) => {
     mongoose.connect('mongodb://localhost/books_test', {
--- test/create_test.js
+++ test/create_test.js
@@ -2,8 +2,11 @@ const assert = require('assert');
 const Book = require('../src/books');
 
 describe('Create book', () => {
-    it('Save a book', () => {
+    it('Save a book', (done) => {
         const book1 = new Book({title: "Game of thrones"});
-        book1.save();
+        book1.save().then(() => {
+            assert(!book1.isNew);
+            done();
+        });
     })
 })
