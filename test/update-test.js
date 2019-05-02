45581670652e40db83887eb331810229b6985242
sylvian <sylvian31@gmail.com>
2019-05-02 04:38:55 -0400

    update test

---
+++ test/update-test.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const Book = require('../src/books');
+
+describe('Test update', () => {
+    let book1;
+    let newTitle = 'Le seigneur des anneaux';
+    beforeEach((done) => {
+        book1 = new Book({ title: "Game of thrones" });
+        book1.save().then(() => {
+            done();
+        });
+    })
+
+    function assertTitle(promise, done) {
+        promise.then(() => {
+            Book.findOne({}).then((book) => {
+                assert(book.title === newTitle);
+                done();
+            })
+        })
+    }
+
+    it('update a book from instance', (done) => {
+        book1.set('title', newTitle);
+        assertTitle(book1.save(), done);
+    });
+
+    it('update a book from model', (done) => {
+        assertTitle(Book.update({ title: 'Game of thrones' }, { title: newTitle }), done);
+    });
+
+    it('find one by title and update (findOneAndUpdate)', (done) => {
+        assertTitle(Book.findOneAndUpdate({ title: 'Game of thrones' }, { title: newTitle }), done);
+    })
+
+    it('find one by id and update (findByIdAndUpdate)', (done) => {
+        assertTitle(Book.findByIdAndUpdate(book1._id, { title: newTitle }), done);
+    })
+
+})
